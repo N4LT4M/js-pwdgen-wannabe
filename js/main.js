01a710fcc1114c9aaebc02157b05579bca51220a
N4LT4M <m4tt3o.casale@gmail.com>
2023-06-29 15:50:09 +0200

    impostazione e formazione dei prompt

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla console nomecognomecolorepreferito21
+
+salvare informazioni dell'utente tramite variabile let e prompt
+
+- nome
+- cognome
+- colore preferito
+
+unire le stringe e aggiungere 21 usando l'operatore di concatenazione +  */
+
+//inizio
+let nameUser = prompt("qual'è il tuo nome?")
+let surnameUser = prompt("qual'è il tuo cognome?")
+let favoritecolorUser = prompt("qual'è il tuo colore preferito?")
